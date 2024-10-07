2d693e38e09715ceea93910a3e4dc48aeb1fbd1a
Alen Bluberry <162904247+AlenBluBerry@users.noreply.github.com>
2024-10-07 13:23:23 +0530

    Custom_String_List_Generator.py

--- Custom_String_List_Generator.py
+++ Custom_String_List_Generator.py
@@ -3,6 +3,6 @@ store_value= int(input('Enter how many you need to store in the list: '))
 
 for i in range(store_value):
     val = str(input('Enter the value: '))  # Move input inside the loop
-    l.append(val)
+    list.append(val)
 
 print(list)
