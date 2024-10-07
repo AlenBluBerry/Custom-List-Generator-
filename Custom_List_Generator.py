d0a21d455052f598f7c144fa5ffc95a699bd01e6
Alen Bluberry <162904247+AlenBluBerry@users.noreply.github.com>
2024-10-07 13:25:29 +0530

    Custom_Integer_List_Generator.py

--- Custom_List_Generator.py
+++ Custom_List_Generator.py
@@ -1,8 +1,9 @@
+# Only integer can be entered 
 list = []
 store_value = int(input('Enter how many you need to store in the list: '))
 
 for i in range(store_value):
     val = int(input('Enter the value: '))  # Move input inside the loop
-    l.append(val)
+    list.append(val)
 
 print(list)
