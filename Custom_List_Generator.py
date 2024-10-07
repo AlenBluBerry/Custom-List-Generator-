6e2025360857f1c2e56cc484ccbaf484e05a8e8b
Alen Bluberry <162904247+AlenBluBerry@users.noreply.github.com>
2024-10-07 13:04:04 +0530

    Custom_List_Generator.py

---
+++ Custom_List_Generator.py
@@ -0,0 +1,8 @@
+list = []
+store_value = int(input('Enter how many you need to store in the list: '))
+
+for i in range(store_value):
+    val = int(input('Enter the value: '))  # Move input inside the loop
+    l.append(val)
+
+print(list)
